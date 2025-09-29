e573318b3debfbfdc3fd643ae4fccb498e854d52
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-09-29 14:25:50 +0700

    feat/simulation-api-changes

--- src/app/apis/SimulationManager/SimulationManagerPracticeApi.js
+++ src/app/apis/SimulationManager/SimulationManagerPracticeApi.js
@@ -1,43 +1,45 @@
-// src\app\apis\SimulationManager\SimulationManagerPracticeApi.js
+// src/app/apis/SimulationManager/SimulationManagerPracticeApi.js
 
 import axios from 'axios';
 
-// Use env variable for the API base
-const API_BASE = `${import.meta.env.VITE_API_Simulation_Service_URL}/Practices`;
+// Only use the base URL from env
+const API_BASE = import.meta.env.VITE_API_Simulation_Service_URL;
 
-// Practices CRUD
+// --- Practices CRUD --- //
 export const getPractices = (page = 1, pageSize = 10) =>
-  axios.get(API_BASE, { params: { page, pageSize } }).then(res => res.data);
+  axios.get(`${API_BASE}/practices`, { params: { page, pageSize } }).then(res => res.data);
 
 export const getPracticeById = (id) =>
-  axios.get(`${API_BASE}/${id}`).then(res => res.data);
+  axios.get(`${API_BASE}/practices/${id}`).then(res => res.data);
 
 export const createPractice = (data) =>
-  axios.post(API_BASE, data).then(res => res.data);
+  axios.post(`${API_BASE}/practices`, data).then(res => res.data);
 
 export const updatePractice = (id, data) =>
-  axios.put(`${API_BASE}/${id}`, data).then(res => res.data);
+  axios.put(`${API_BASE}/practices/${id}`, data).then(res => res.data);
 
 export const deletePractice = (id) =>
-  axios.delete(`${API_BASE}/${id}`).then(res => res.data);
+  axios.delete(`${API_BASE}/practices/${id}`).then(res => res.data);
 
-// --- PracticeStep APIs ---
+// --- PracticeStep APIs --- //
 
+// Get all steps for a practice (nếu còn endpoint này, vẫn giữ như cũ)
 export const getPracticeStepsByPracticeId = (practiceId) =>
-  axios.get(`${API_BASE}/${practiceId}/steps`).then(res => res.data);
+  axios.get(`${API_BASE}/PracticeSteps/practice/${practiceId}`).then(res => res.data);
 
+// Get single step by id (now: /api/PracticeSteps/:id)
 export const getPracticeStepById = (stepId) =>
-  axios.get(`${API_BASE}/steps/${stepId}`).then(res => res.data);
+  axios.get(`${API_BASE}/PracticeSteps/${stepId}`).then(res => res.data);
 
+// Create step (kiểm tra lại endpoint, thường là POST /PracticeSteps)
 export const createPracticeStep = (data) =>
-  axios.post(`${API_BASE}/steps`, data).then(res => res.data);
+  axios.post(`${API_BASE}/PracticeSteps`, data).then(res => res.data);
 
+// Update step
 export const updatePracticeStep = (stepId, data) =>
-  axios.put(`${API_BASE}/steps/${stepId}`, data).then(res => res.data);
+  axios.put(`${API_BASE}/PracticeSteps/${stepId}`, data).then(res => res.data);
 
+// Delete step
 export const deletePracticeStep = (stepId) =>
-  axios.delete(`${API_BASE}/steps/${stepId}`).then(res => res.data);
-
-
-
+  axios.delete(`${API_BASE}/PracticeSteps/${stepId}`).then(res => res.data);
 
