b5456d19083b3fd598753b4a7fdb52c9c8528560
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-12-09 22:00:43 +0700

    fix/axios-error-navigate

--- src/app/libs/axios.js
+++ src/app/libs/axios.js
@@ -36,12 +36,12 @@ apiClient.interceptors.response.use(
     const status = error?.response?.status;
     if (status === 401) {
       try {
-        removeAuthToken();
+        // removeAuthToken();
       } catch (err) {}
-      if (typeof window !== 'undefined') {
-        // ensure full reload to clear state
-        window.location.replace('/auth/login');
-      }
+      // if (typeof window !== 'undefined') {
+      //   // ensure full reload to clear state
+      //   window.location.replace('/auth/login');
+      // }
     }
     return Promise.reject(error);
   }
